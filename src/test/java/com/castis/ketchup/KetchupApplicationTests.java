333939cc3f35271c1133b8260eabd64a1174e1c7
jonghwashin <tls1403@naver.com>
2023-03-30 13:56:51 +0900

    API 생성

--- src/test/java/com/castis/ketchup/KetchupApplicationTests.java
+++ src/test/java/com/castis/ketchup/KetchupApplicationTests.java
@@ -3,10 +3,10 @@ package com.castis.ketchup;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class KetchupApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
